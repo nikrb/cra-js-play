e698e65bfaded68acac630a47136694a821f168c
nikrb <nik_rb@yahoo.com>
2018-02-22 21:43:36 +0000

    check image metrix after <input> file selection

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  fileChange = e => {
+    const file = e.target.files[0];
+    let img;
+    if (file) {
+      img = new Image();
+      img.onload = () => {
+          console.log(img.width + " " + img.height);
+      };
+      img.src = URL.createObjectURL(file);
+    }
+  };
   render() {
     return (
       <div className="App">
@@ -13,6 +24,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="file" onChange={this.fileChange} />
       </div>
     );
   }
